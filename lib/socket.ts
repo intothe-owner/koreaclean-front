61f92dd60a13a98c61f2ad8c64e7d1eb732b3a19
intothe-owner <bintothe@daum.net>
2025-11-14 09:21:13 +0900

    도메인 설정

--- lib/socket.ts
+++ lib/socket.ts
@@ -6,7 +6,7 @@ let _socket: Socket | null = null;
 function pickBaseURL() {
   if (typeof window === "undefined") return "/";
   //return "http://113.131.151.103:4500"; // 백엔드 직접 접속
-  return "http://3.36.49.217"; // 백엔드 직접 접속
+  return "http://back.koreacoop.kr"; // 백엔드 직접 접속
 }
 
 function getAccessToken(): string | undefined {
